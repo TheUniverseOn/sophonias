3a9c615fc375cfda0272be383668dab0e5471f9c
TheUniverseOn <sophonias8@gmail.com>
2020-05-03 18:38:43 +0100

    Add gatsby plugin and sharp library

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,21 @@ module.exports = {
         path: `${__dirname}/src/images`
       },
     },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, 
+    {
+      resolve: `gatsby-plugin-mdx`, 
+      options: {
+        extenstion: [`.md`, `.mdx`], 
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`, 
+            options: {
+              maxWidth: 1200, 
+            }, 
+          }, 
+        ]
+      }, 
+    }, 
   ],
 }
