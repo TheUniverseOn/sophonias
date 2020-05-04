ab5612096acb34726ebab3857e211a74346f30ed
TheUniverseOn <sophonias8@gmail.com>
2020-05-04 18:19:24 +0100

    Added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,5 +40,15 @@ module.exports = {
         ]
       }, 
     }, 
+    {
+      resolve: `gatsby-plugin-google-fonts`, 
+      options: {
+        fonts:[
+          `roboto mono`, 
+          `muli\:400,400i,700, 700i`,
+        ], 
+        display: "swap", 
+      },
+    },
   ],
 }
